52d6d9c6aae42a0460f1d53ce43a24f8cae82014
Brian Bastian <brianbastian1989@gmail.com>
2023-09-05 10:28:36 +0100

    created a sample delegate. How delegate works

---
+++ delegate-demo/Program.cs
@@ -0,0 +1,33 @@
+﻿public delegate void Delegate(string msg); // declaring a delegate
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Delegate del = ClassA.MethodA;
+        del("Hello World");
+
+        del = ClassB.MethodB;
+        del("Hello World");
+
+        del = (string msg) => Console.WriteLine("Called lambda expression: " + msg);
+        del("Hello World");
+        Console.ReadLine();
+    }
+}
+
+public class ClassA
+{
+    public static void MethodA(string msg)
+    {
+        Console.WriteLine("Called ClassA.MethodA() with parameter: " + msg);
+    }
+}
+
+public class ClassB
+{
+    public static void MethodB(string msg)
+    {
+        Console.WriteLine("Called ClassB.MethodB() with parameter: " + msg);
+    }
+}
